fda4ed4e871775ec878470e278237d79fc5f9389
Asha Karanam <asresha42@gmail.com>
2020-05-12 21:47:03 -0500

    Initial Commit

---
+++ A1g.py
@@ -0,0 +1,5 @@
+mynameisAsha = "Camel case"
+MyNameIsAsha = "Upper Case"
+my_name_is_asha = "Ladder Case"
+
+print(mynameisAsha, '-mynameisAsha\n', MyNameIsAsha, '-MyNameIsAsha\n', my_name_is_asha, '-my_name_is_asha')
